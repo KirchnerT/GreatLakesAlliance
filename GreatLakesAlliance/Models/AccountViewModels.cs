a5826b60e6d337fd2821d4185fa53511a7f0d93c
Tyler Kirchner <kirchner3017@uwlax.edu>
2020-02-29 13:27:15 -0600

    COMMIT/02292020-MAJOR UPDATES

--- GreatLakesAlliance/Models/AccountViewModels.cs
+++ GreatLakesAlliance/Models/AccountViewModels.cs
@@ -48,10 +48,14 @@ namespace GreatLakesAlliance.Models
 
     public class LoginViewModel
     {
-        [Required]
+        //[Required]
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Display(Name ="User Name")]
+        [Required]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -68,6 +72,10 @@ namespace GreatLakesAlliance.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+        [Display(Name = "User name"), Required]
+        public string UserName { get; set; }
+        [Display(Name = "Full Name"), Required]
+        public string FullName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
