1db5a2d2f5e8f27fce0c266eb823609555199d68
Tyler Kirchner <kirchner3017@uwlax.edu>
2020-03-02 13:31:03 -0600

    COMMIT/03022020-Adding donation screens

---
+++ GreatLakesAlliance/Controllers/DonorController.cs
@@ -0,0 +1,55 @@
+﻿using GreatLakesAlliance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreatLakesAlliance.Controllers
+{
+    public class DonorController : Controller
+    {
+        // GET: Donor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Donor/PrevDonations/5
+        public ActionResult PrevDonations()   //add (int d) for id for the person donating to grab their previous donations
+        {
+            var prevDonations = new List<DonorDataModel>
+            {
+                new DonorDataModel() { amount = 100, cardNum = 1111111111111111, ccv = 111, expDate = "12/20", fullName = "John Johnson" },
+                new DonorDataModel() { amount = 345, cardNum = 9348759384579873, ccv = 273, expDate = "04/20", fullName = "John Johnson" },
+                new DonorDataModel() { amount = 100, cardNum = 1111111111111111, ccv = 111, expDate = "12/20", fullName = "John Johnson" }
+
+            };
+            return View(prevDonations);
+        }
+
+        // GET: Donor/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Donor/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+    }
+}
---
+++ GreatLakesAlliance/Models/DonorDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GreatLakesAlliance.Models
+{
+    public class DonorDataModel
+    {
+        public string fullName { get; set; }
+        public long cardNum { get; set; }
+        public string expDate { get; set; }
+        public int ccv { get; set; }
+        public int amount { get; set; }
+
+    }
+}
