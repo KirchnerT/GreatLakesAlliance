37e215566e07f7895176428d1e67d64498d4c62c
Tyler Kirchner <kirchner3017@uwlax.edu>
2020-03-30 14:46:53 -0500

    COMMIT-localdb table scripts in solution

---
+++ GLADatabase/dbo/Tables/AspNetDonations.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[AspNetDonations] (
+    [Id]             NVARCHAR (50) NOT NULL,
+    [cardNumber]     NVARCHAR (30) NOT NULL,
+    [expirationDate] NVARCHAR (10) NOT NULL,
+    [ccv]            NVARCHAR (3)  NOT NULL,
+    [amount]         INT           NOT NULL,
+    [orgEvent]       NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ GLADatabase/dbo/Tables/AspNetEventData.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[AspNetEventData] (
+    [eventId]          NVARCHAR (128) NOT NULL,
+    [eventName]        NVARCHAR (128) NOT NULL,
+    [eventStartDate]   NVARCHAR (10)  NOT NULL,
+    [eventEndDate]     NVARCHAR (10)  NULL,
+    [volunteersNeeded] INT            NULL,
+    [location]         NVARCHAR (256) NULL,
+    [startTime]        NVARCHAR (10)  NULL,
+    [endTime]          NVARCHAR (10)  NULL,
+    [description]      NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([eventId] ASC)
+);
+
