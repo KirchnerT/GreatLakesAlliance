927f6b1e57ae5de8c666c0ad55a2adea931a4ea7
Tyler Kirchner <kirchner3017@uwlax.edu>
2020-05-04 21:06:17 -0500

    COMMIT

--- GreatLakesAlliance/Controllers/ReportsController.cs
+++ GreatLakesAlliance/Controllers/ReportsController.cs
@@ -31,17 +31,124 @@ namespace GreatLakesAlliance.Controllers
 
         public ActionResult Volunteers()
         {
-            return View();
+            List<VolunteeredEventsModel> volunteers = db.VolunteeredEventsModel.ToList();
+
+            List<VolunteerModel> realList = new List<VolunteerModel>();
+
+            foreach (var item in volunteers)
+            {
+                VolunteerModel tempVol = new VolunteerModel();
+
+                tempVol.userId = item.UserId;
+                tempVol.eventId = item.EventId;
+                tempVol.fullName = item.FullName;
+
+                string eName = db.EventDataModels.Where(a => a.eventId == item.EventId).Select(a => a.eventName).First();
+                tempVol.eventName = eName;
+                
+
+                realList.Add(tempVol);
+            }
+
+            realList = realList.OrderBy(a => a.eventName).ToList();
+
+            return View(realList);
         }
 
         public ActionResult Donations()
         {
-            return View();
+            List<DonorDataModel> donators = db.DonorDataModels.OrderBy(a => a.fullName).ToList();
+
+            List<DonorModel> realDonors = new List<DonorModel>();
+
+            foreach (var item in donators)
+            {
+                DonorModel tempDonor = new DonorModel();
+
+                tempDonor.eventId = item.eventId;
+                tempDonor.amount = item.amount;
+                tempDonor.fullName = item.fullName;
+                tempDonor.userId = item.userId;
+
+                string eName = "Great Lakes Alliance";
+                if (!(item.eventId == 0))
+                {
+                    eName = db.EventDataModels.Where(a => a.eventId == item.eventId).Select(a => a.eventName).First(); tempDonor.eventName = eName;
+                }
+
+                tempDonor.eventName = eName;
+
+                realDonors.Add(tempDonor);
+            }
+
+            realDonors = realDonors.OrderBy(a => a.eventName).ToList();
+
+            return View(realDonors);
         }
 
         public ActionResult Users()
         {
-            return View();
+
+            List<ApplicationUser> users = db.Users.OrderBy(a => a.FullName).ToList();
+            List<UserModel> realUsers = new List<UserModel>();
+
+            foreach (var item in users)
+            {
+                UserModel tempUser = new UserModel();
+                tempUser.FullName = item.FullName;
+                tempUser.Email = item.Email;
+                tempUser.Id = item.Id;
+
+                realUsers.Add(tempUser);
+            }
+
+            return View(realUsers);
+        }
+
+        public ActionResult UserData(string userId)
+        {
+            UserData userData = new UserData();
+
+            List<VolunteeredEventsModel> volunteered = db.VolunteeredEventsModel.Where(a => a.UserId == userId).ToList();
+            List<DonorDataModel> donations = db.DonorDataModels.Where(a => a.userId == userId).ToList();
+
+            List<ShortVolunteer> allVol = new List<ShortVolunteer>();
+            List<ShortDonation> allDonations = new List<ShortDonation>();
+
+            foreach (var item in volunteered)
+            {
+                ShortVolunteer temp = new ShortVolunteer();
+
+                string eName = db.EventDataModels.Where(a => a.eventId == item.EventId).Select(a => a.eventName).First();
+                temp.eventName = eName;
+
+                allVol.Add(temp);
+
+            }
+
+            foreach (var item in donations)
+            {
+                ShortDonation temp = new ShortDonation();
+
+                temp.amount = item.amount;
+                temp.cardNumber = item.cardNumber;
+
+                string eName = "Great Lakes Alliance";
+                if (!(item.eventId == 0))
+                {
+                    eName = db.EventDataModels.Where(a => a.eventId == item.eventId).Select(a => a.eventName).First();
+                }
+                temp.eventName = eName;
+
+                allDonations.Add(temp);
+            }
+
+            userData.donations = allDonations;
+            userData.volunteer = allVol;
+
+            ViewBag.name = db.Users.Where(a => a.Id == userId).Select(a => a.FullName).First();
+
+            return View(userData);
         }
     }
 }
---
+++ GreatLakesAlliance/Models/ReportModels.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GreatLakesAlliance.Models
+{        
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+    }
+
+    public class VolunteerModel
+    {
+        public string userId { get; set; }
+        public int eventId { get; set; }
+        public string fullName { get; set; }
+        public string eventName { get; set; }
+    }
+
+    public class DonorModel
+    {
+        public string userId { get; set; }
+        public int eventId { get; set; }
+        public string eventName { get; set; }
+        public string fullName { get; set; }
+        public int amount { get; set; }
+    }
+
+    public class UserData
+    {
+        public List<ShortDonation> donations { get; set; }
+        public List<ShortVolunteer> volunteer { get; set; }
+    }
+
+    public class ShortDonation
+    {
+        public string eventName { get; set; }
+        public int amount { get; set; }
+        public string cardNumber { get; set; }
+    }
+
+    public class ShortVolunteer
+    {
+        public string eventName { get; set; }
+    }
+}
